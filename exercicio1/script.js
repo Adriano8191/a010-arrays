43029f7bfd0526b7383ad27e3287da09caac5336
adriano <adriano1766@hotmail.com>
2022-09-07 21:14:06 -0300

    Exercicios feitos

---
+++ exercicio1/script.js
@@ -0,0 +1,18 @@
+let arrayNumber = [5, 10, 37, 17, 8]
+let arrayString = ['Banana', 'Uva', 'Manga', 'Maça', 'Limão']
+let arrayDiversos = ['Banana', 7, true]
+
+console.log(arrayNumber)
+console.log(arrayString)
+console.log(arrayDiversos)
+
+console.log(arrayNumber.length)
+console.log(arrayString.length)
+console.log(arrayDiversos.length)
+
+console.log(arrayNumber[0])
+console.log(arrayString[0])
+console.log(arrayDiversos[0])
+
+console.log(arrayNumber.includes(5))
+console.log(arrayDiversos.includes(8))
